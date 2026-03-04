5d88c0a9cc30bda793a7a0f69f0d7cf40d71ddfd
Andrea López Palomeque <andrealopezpalomeque@gmail.com>
2026-03-04 15:51:11 -0300

    disable firebase credentials for now

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,16 +7,16 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@nuxt/icon',
     'dayjs-nuxt',
-    'nuxt-vuefire',
+    // 'nuxt-vuefire', // TODO: enable once Firebase credentials are configured in .env
   ],
-  vuefire: {
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-    },
-  },
+  // vuefire: {
+  //   config: {
+  //     apiKey: process.env.FIREBASE_API_KEY,
+  //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  //     projectId: process.env.FIREBASE_PROJECT_ID,
+  //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  //     appId: process.env.FIREBASE_APP_ID,
+  //   },
+  // },
 })
